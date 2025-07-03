8da6524e42bc83c0aab9fbaed95ea344686ea75a
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-03 22:57:25 +0800

    I think I've finished before to watch the class, now I'm going to watch to see if they did something diferent. I still need to add functionality to the restart button.

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+let palavraSorteado = quantidade > 1 ? 'Numeros sorteados' : 'Numero sorteado';
+
 function sortear () {
     let quantidade = document.getElementById('quantidade').value;
     let de = document.getElementById('de').value;
     let ate = document.getElementById('ate').value;
 
-    alert(`quantidade: ${quantidade}`);
-    alert(`Comeca de: ${de}`);
-    alert(`Vai ate: ${ate}`);
+    // alert(`quantidade: ${quantidade}`);
+    // alert(`Comeca de: ${de}`);
+    // alert(`Vai ate: ${ate}`);
 
     listaSorteados = [];
     while (listaSorteados.length < quantidade) {
-    let numSorteado = parseInt(Math.random() * (ate - de + 1));
+    let numSorteado = parseInt(Math.random() * (ate - de + 1)) + 1;
     listaSorteados.push(numSorteado);
-    alert(`Lista sorteados: ${listaSorteados}`);
+    // alert(`Lista sorteados: ${listaSorteados}`);
     }
+
+    mostrarTextoResultado();
 }
 
 
-function mostrarTexto() {
-    
+
+function mostrarTextoResultado() {
+    let labelResultado = document.getElementById('resultado').querySelector('label');
+    return labelResultado.textContent = `${palavraSorteado}:  ${listaSorteados}`;
 }
