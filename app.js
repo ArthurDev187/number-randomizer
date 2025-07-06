fbdbd38125d421bcd052fae87bc439c01d211872
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-06 17:45:28 +0800

    I'm trying to do everything again to practice

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-let palavraSorteado = quantidade > 1 ? 'Numeros sorteados' : 'Numero sorteado';
+let quantidade = document.getElementById('quantidade');
+let de = document.getElementById('de');
+let ate = document.getElementById('ate');
+let listaSorteados = [];
 
-function sortear () {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
-
-    // alert(`quantidade: ${quantidade}`);
-    // alert(`Comeca de: ${de}`);
-    // alert(`Vai ate: ${ate}`);
-
-    listaSorteados = [];
-    while (listaSorteados.length < quantidade) {
-    let numSorteado = parseInt(Math.random() * (ate - de + 1)) + 1;
-    listaSorteados.push(numSorteado);
-    // alert(`Lista sorteados: ${listaSorteados}`);
-    }
-
-    mostrarTextoResultado();
-}
-
-
-
-function mostrarTextoResultado() {
-    let labelResultado = document.getElementById('resultado').querySelector('label');
-    return labelResultado.textContent = `${palavraSorteado}:  ${listaSorteados}`;
+function sortear() {
+    numeroSo
 }
