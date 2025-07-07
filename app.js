ffae3ebb38d51793297797fad33315ef37bd50e7
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-07-07 21:14:31 +0800

    doing all it over again for practicing

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-let quantidade = document.getElementById('quantidade');
-let de = document.getElementById('de');
-let ate = document.getElementById('ate');
-let listaSorteados = [];
-
 function sortear() {
-    numeroSo
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de  = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    let listaSorteados = []
+    let numeroSorteado
+
+    for (let i = 0; i < quantidade; i++) {
+        numeroSorteado = obterNumeroAleatorio(de, ate);
+        while (listaSorteados.includes(numeroSorteado)) {
+            numeroSorteado = obterNumeroAleatorio(de, ate);
+        }
+        listaSorteados.push(numeroSorteado);
+    }
+    exibirMensagemNaTela(listaSorteados)
+}
+
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function exibirMensagemNaTela(listaNumeros) {
+    let mensagemResultado = document.getElementById('resultado').querySelector('label')
+    if (mensagemResultado.textContent == 'Números sorteados:  nenhum até agora') {
+        mensagemResultado.textContent = `Números sorteados:  ${listaNumeros}`
+    } else {
+        mensagemResultado.textContent == 'Números sorteados:  nenhum até agora'
+    }
+    
 }
